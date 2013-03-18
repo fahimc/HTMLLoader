c4f06ebe21becbff134c788671b626bdf231bc30
unknown <fahim.chowdhury@.omega.wwads.net>
2013-03-18 16:23:29 +0000

    get link null bug

--- build/src/HTMLLoader.js
+++ build/src/HTMLLoader.js
@@ -6,7 +6,7 @@ var HTMLLoader = {
 	handler : {},
 	scripts : [],
 	links : [],
-	callback:null,
+	callback : null,
 	content : {
 		body : null
 	},
@@ -16,16 +16,17 @@ var HTMLLoader = {
 		this.handler.onHTMLLoad = function(t, x) {
 			root.onHTMLLoad(t, x);
 		}
-		Utensil.URLLoader.load(url+"?rand="+Math.random(), this.handler.onHTMLLoad);
+		Utensil.URLLoader.load(url + "?rand=" + Math.random(), this.handler.onHTMLLoad);
 	},
 	onHTMLLoad : function(t, x) {
 		this.setBody(t);
 		this.setScriptTags(t);
 		this.setLinkTags(t);
-		if(this.callback)this.callback(this.content.body);
+		if (this.callback)
+			this.callback(this.content.body);
 	},
 	setBody : function(t) {
-		t = t.replace(/(\r\n|\n|\r)/gm,"");
+		t = t.replace(/(\r\n|\n|\r)/gm, "");
 		var regBody = /<body[^>]*>|<\/body>/gi;
 		var body = (t.split( regBody )[1]);
 		this.content.body = body;
@@ -36,7 +37,7 @@ var HTMLLoader = {
 		var s = t.match(regScript);
 		var re_src = /src="(.*?)"/i;
 		this.scripts = this.getTagLinks(s, re_src, 'src="');
-		this.addLinkToHead("script", this.scripts, this.createScriptTag,"src");
+		this.addLinkToHead("script", this.scripts, this.createScriptTag, "src");
 	},
 	createScriptTag : function(link) {
 		var sct = document.createElement('script');
@@ -49,7 +50,7 @@ var HTMLLoader = {
 		var s = t.match(regLink);
 		var re_src = /href="(.*?)"/i;
 		this.links = this.getTagLinks(s, re_src, 'href="');
-		this.addLinkToHead("link", this.links, this.createLinkTag,"href");
+		this.addLinkToHead("link", this.links, this.createLinkTag, "href");
 	},
 	createLinkTag : function(link) {
 		var sct = document.createElement('link');
@@ -58,7 +59,7 @@ var HTMLLoader = {
 		sct.rel = "stylesheet";
 		return sct;
 	},
-	addLinkToHead : function(tagName, arr, func,att) {
+	addLinkToHead : function(tagName, arr, func, att) {
 		var tags = document.getElementsByTagName(tagName);
 
 		for (var b = 0; b < arr.length; b++) {
@@ -78,31 +79,35 @@ var HTMLLoader = {
 	},
 	getTagLinks : function(arr, reg, rep) {
 		var elems = [];
-		for (var a = 0; a < arr.length; a++) {
+		if (arr) {
+			for (var a = 0; a < arr.length; a++) {
 
-			var src = arr[a].match(reg);
-			if (src[0]) {
-				src = src[0].replace(rep, "");
-				src = src.replace('"', "");
+				var src = arr[a].match(reg);
+				if (src[0]) {
+					src = src[0].replace(rep, "");
+					src = src.replace('"', "");
+				}
+				elems.push(src);
 			}
-			elems.push(src);
 		}
+
 		return elems;
 	},
-	absolute:function(base, relative) {
-		if(relative.indexOf("http")>=0)return relative;
-    var stack = base.split("/"),
-        parts = relative.split("/");
-    stack.pop(); // remove current file name (or empty string)
-                 // (omit if "base" is the current folder without trailing slash)
-    for (var i=0; i<parts.length; i++) {
-        if (parts[i] == ".")
-            continue;
-        if (parts[i] == "..")
-            stack.pop();
-        else
-            stack.push(parts[i]);
-    }
-    return stack.join("/");
-}
+	absolute : function(base, relative) {
+		if (relative.indexOf("http") >= 0)
+			return relative;
+		var stack = base.split("/"), parts = relative.split("/");
+		stack.pop();
+		// remove current file name (or empty string)
+		// (omit if "base" is the current folder without trailing slash)
+		for (var i = 0; i < parts.length; i++) {
+			if (parts[i] == ".")
+				continue;
+			if (parts[i] == "..")
+				stack.pop();
+			else
+				stack.push(parts[i]);
+		}
+		return stack.join("/");
+	}
 };
